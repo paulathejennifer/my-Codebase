163d7be7b205ee7178d4a58ab9733486b6b0c48a
paulathejennifer <paulathejennifer@gmail.com>
2025-06-03 14:29:29 +0300

    Library system set  up in Object Oriented Programming

---
+++ library.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title, isbn, author):
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+        self.is_available = True
+
+
+# class Member:
+#     def __init__(self,name,id):
+#         self.name = name
+#         self.id = id
+#         self.
+
+# class Library:
