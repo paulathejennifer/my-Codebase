f17f776cc2d01698e84f192260bdb04f8248c0e5
paulathejennifer <paulathejennifer@gmail.com>
2025-06-03 15:45:34 +0300

    Hospital system set up: class Doctor and attributes: name, specialization

--- library.py
+++ library.py
@@ -57,7 +57,7 @@ class Patient:
 class Doctor:
     def __init__(self, name, specialization):
         self.name = name
-# #         self.specialization = specialization
+        self.specialization = specialization
 # #         self.available = True
 
 
